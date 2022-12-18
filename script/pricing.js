927ed819e1666f64bf50481e5adb116722b29fd9
madhurn29 <madhur.nadamwar29@gmail.com>
2022-12-18 20:22:06 +0530

    navigation done

--- script/pricing.js
+++ script/pricing.js
@@ -2,3 +2,12 @@ let pricingBtn = document.getElementById("pricingBtn")
 pricingBtn.addEventListener("click", function(){
     window.location.href="../pages/Pricing.html"
 })
+
+let customerBtn = document.getElementById("customerBtn")
+customerBtn.addEventListener("click", function(){
+    window.location.href="../pages/customers.html"
+})
+let partnerBtn = document.getElementById("partnerBtn")
+partnerBtn.addEventListener("click", function(){
+    window.location.href="../pages/partners.html"
+})
