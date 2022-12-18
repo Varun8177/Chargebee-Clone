1e354c4e8fd76702725c9bd1d8775ed1588d19b5
madhurn29 <madhur.nadamwar29@gmail.com>
2022-12-19 00:37:13 +0530

    final

--- script/customer.js
+++ script/customer.js
@@ -14,3 +14,15 @@ let partnerBtn = document.getElementById("partnerBtn")
 partnerBtn.addEventListener("click", function(){
     window.location.href="../pages/partners.html"
 })
+
+
+let loginBtn= document.getElementById("login")
+loginBtn.addEventListener("click", function(){
+    
+    window.location.href="../pages/login.html"
+})
+
+let demoBtn= document.getElementById("demoBtn")
+demoBtn.addEventListener("click", function(){
+    window.location.href="../pages/demo.html"
+})
--- script/partners.js
+++ script/partners.js
@@ -12,3 +12,17 @@ let partnerBtn = document.getElementById("partnerBtn")
 partnerBtn.addEventListener("click", function(){
     window.location.href="../pages/partners.html"
 })
+
+
+
+
+let loginBtn= document.getElementById("login")
+loginBtn.addEventListener("click", function(){
+    
+    window.location.href="../pages/login.html"
+})
+
+let demoBtn= document.getElementById("demoBtn")
+demoBtn.addEventListener("click", function(){
+    window.location.href="../pages/demo.html"
+})
