b299658e25d27d0ec196e88a1529dcc831efc7fd
Madhur Nadamwar <112754729+madhurn29@users.noreply.github.com>
2023-04-11 00:17:47 +0530

    login btn added

--- script/index.js
+++ script/index.js
@@ -1,35 +1,40 @@
 //* button to navigate to pricing page
 
-let pricingBtn = document.getElementById("pricingBtn")
-pricingBtn.addEventListener("click", function(){
-    window.location.href="../permissible-step-1626/pages/Pricing.html"
-})
+let pricingBtn = document.getElementById("pricingBtn");
+pricingBtn.addEventListener("click", function () {
+  window.location.href = "../permissible-step-1626/pages/Pricing.html";
+});
 
-let customerBtn = document.getElementById("customerBtn")
-customerBtn.addEventListener("click", function(){
-    window.location.href="./pages/customers.html"
-})
-let partnerBtn = document.getElementById("partnerBtn")
-partnerBtn.addEventListener("click", function(){
-    window.location.href="./pages/partners.html"
-})
+let customerBtn = document.getElementById("customerBtn");
+customerBtn.addEventListener("click", function () {
+  window.location.href = "./pages/customers.html";
+});
+let partnerBtn = document.getElementById("partnerBtn");
+partnerBtn.addEventListener("click", function () {
+  window.location.href = "./pages/partners.html";
+});
 
-let loginBtn= document.getElementById("login")
-loginBtn.addEventListener("click", function(){
-    window.location.href="./pages/login.html"
-})
+let loginBtn = document.getElementById("login");
+loginBtn.addEventListener("click", function () {
+  window.location.href = "./pages/login.html";
+});
 
-let demoBtn= document.getElementById("demo-Btn-nav")
-demoBtn.addEventListener("click", function(){
-    window.location.href="./pages/demo.html"
-})
+let demoBtn = document.getElementById("demo-Btn-nav");
+demoBtn.addEventListener("click", function () {
+  window.location.href = "./pages/demo.html";
+});
 
-let signUpBtn= document.getElementById("sign-up-btn")
-signUpBtn.addEventListener("click", function(){
-    window.location.href="./pages/signup.html"
-})
+let signUpBtn = document.getElementById("sign-up-btn");
+signUpBtn.addEventListener("click", function () {
+  window.location.href = "./pages/signup.html";
+});
 
-let sub_demoBtn = document.getElementById("sub_demo")
-sub_demoBtn.addEventListener("click", function(){
-    window.location.href="./pages/demo.html"
-})
+let sub_demoBtn = document.getElementById("sub_demo");
+sub_demoBtn.addEventListener("click", function () {
+  window.location.href = "./pages/demo.html";
+});
+
+let navdemoBtn = document.getElementById("demo_btn");
+navdemoBtn.addEventListener("click", function () {
+  window.location.href = "./pages/demo.html";
+});
